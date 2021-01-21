791bab9d9379c167f9feb6ad1313f54580cad09a
jason5327 <js5327@hotmail.com>
2021-01-21 21:41:28 +0800

    Lab2-1

--- src/jenkinsfile/ProjectA-stage.groovy
+++ src/jenkinsfile/ProjectA-stage.groovy
@@ -2,18 +2,31 @@ import hudson.model.*;
 
 
 pipeline{
-	
-	agent any
-	stages{
-		stage("Hello Pipeline") {
-			steps{
-				script {
-					println "Hello Pipeline!"
-					println env.JOB_NAME
-					println env.BUILD_NUMBER
-				}
-			}
-		}
-	}
+
+   agent any
+   stages{
+      stage("Hello Pipeline") {
+         steps {
+             script {
+                println "Hello Pipeline!"
+                println env.JOB_NAME
+                println env.BUILD_NUMBER
+            }
+         }
+      }
+
+      stage("Init paramters in json") {
+         steps {
+             script {
+
+                println "read josn input file"
+                json_file = INPUT_JSON? INPUT_JSON.trim() : ""
+                prop = readJSON file : json_file
+                name = prop.NAME? prop.NAME.trim() : ""
+                println "Name:" + name
+            }
+         }
+      }
+   }
 
 }

01141d7f62d7f7a5ed80468f0cc0304fffb21804
jason5327 <js5327@hotmail.com>
2021-01-22 15:21:08 +0800

    Parameter Test

--- groovy/ProjectA-model.groovy
+++ groovy/ProjectA-model.groovy
@@ -1,10 +1,21 @@
-node('scriptserver') {
-	stage('Stage 1'){
-		echo 'Hello World 1'
-		sh 'df -m'
-	}
-	stage('Stage 2'){
-		echo 'Hello World 2'
-		sh 'ls -l'
-	}
- }
+def getUserInfo(args) {
+	def name = args.name
+	def age = args.age
+	def phone = args.phone
+	def address = args.address
+	def email = args.email
+	def gender = args.gender
+	def is_marry = args.is_marry
+	
+	// she or he
+	def binge = (gender == "male")? "he" : "she"
+	// if is marry
+	def marry = (is_marry == true)? "is marry" : "is not marry yet"
+	def userInfo = """
+	${name} come from ${address}, ${binge} is ${age} old. ${binge}'s phone number is
+	${phone}, or you can contact ${binge} via ${email}, ${binge} ${marry}.
+	"""
+	println userInfo
+}
+ 
+return this;
--- src/jenkinsfile/ProjectA-stage.groovy
+++ src/jenkinsfile/ProjectA-stage.groovy
@@ -21,16 +21,35 @@ pipeline{
       stage("Init paramters in json") {
          steps {
              script {
-
-                println "read josn input file"
-                json_file = INPUT_JSON? INPUT_JSON.trim() : ""
-                prop = readJSON file : json_file
-                name = prop.NAME? prop.NAME.trim() : ""
-                println "Name:" + name
-				println "EMail:" + prop.EMAIL? prop.EMAIL.trim() : ""
+				 println "read josn input file"
+				 json_file = INPUT_JSON? INPUT_JSON.trim() : ""
+				 prop = readJSON file : json_file
+				 name = prop.NAME? prop.NAME.trim() : ""
+				 println "Name:" + name
+				 age = prop.AGE? prop.AGE.trim() : ""
+				 println "Age:" + age
+				 phone = prop.PHONE_NUMBER? prop.PHONE_NUMBER.trim() : ""
+				 println "Phone:" + phone
+				 address = prop.ADDRESS? prop.ADDRESS.trim() : ""
+				 println "Address:" + address
+				 email = prop.EMAIL? prop.EMAIL.trim() : ""
+				 println "Email:" + email
+				 gender = prop.GENDER? prop.GENDER.trim() : ""
+				 println "Gender:" + gender
+				 is_marry = prop.IS_MARRY? prop.IS_MARRY.trim() : false
+				 println "is_marry:" + is_marry
             }
          }
       }
+	   stage("call a method") {
+		   steps {
+			   script {
+				   println "send the parameter as map type"
+				   model_call = load env.WORKSPACE + "/groovy/projectA-model.groovy"
+				   model_call.getUserInfo(name:name, age:age, phone:phone, address:address, email:email, gender:gender, is_marry:is_marry)
+			   }
+		   }
+	   }	  	  
    }
 
 }
